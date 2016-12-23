1f21d857819eb937856169f52ebd609a74631c61
Hannu Lyytikainen <hannu@sharetribe.com>
2016-12-23 12:19:48 +0200

    Add basic auth

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const config = require('./config');
+const auth = require('basic-auth');
 const express = require('express');
 const app = express();
+
+// basic auth
+if (config.env === 'production') {
+  app.use((req, res, next) => {
+    var user = auth(req);
+
+    if (!user || user.name !== config.username || user.pass !== config.password) {
+      res.statusCode = 401;
+      res.setHeader('WWW-Authenticate', 'Basic realm="node-sentry"');
+      res.end('Unauthorized');
+    } else {
+      next();
+    }
+  });
+}
 
 app.get('/', (req, res) => {
   res.send('Sentry node test');
 });
 
-app.listen(config.port, () => console.log(`App started, env: ${config.env}`));
+app.listen(config.port, () => console.log(`App started`));
--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 const config = {
+  env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
-  env: process.env.NODE_ENV || 'development'
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD
 };
 
 module.exports = config;
