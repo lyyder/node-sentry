59f046bfc411addb2313f60fb264734824f87e4c
Hannu Lyytikainen <hannu@sharetribe.com>
2016-12-23 14:36:42 +0200

    Add Sentry integration

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
 const config = require('./config');
 const auth = require('basic-auth');
+const Raven = require('raven');
 const express = require('express');
 const app = express();
 
-// basic auth
-if (config.env === 'production') {
-  app.use((req, res, next) => {
-    var user = auth(req);
+Raven.config(config.sentry.nodeDsn, {
+  environment: config.env,
+  extra: {info: 'NodeJS Sentry test error'}
+}).install();
+app.use(Raven.requestHandler());
 
-    if (!user || user.name !== config.username || user.pass !== config.password) {
-      res.statusCode = 401;
-      res.setHeader('WWW-Authenticate', 'Basic realm="node-sentry"');
-      res.end('Unauthorized');
-    } else {
-      next();
-    }
-  });
-}
+// basic auth
+app.use((req, res, next) => {
+  var user = auth(req);
+
+  if (!user || user.name !== config.username || user.pass !== config.password) {
+    res.statusCode = 401;
+    res.setHeader('WWW-Authenticate', 'Basic realm="node-sentry"');
+    res.end('Unauthorized');
+  } else {
+    next();
+  }
+});
 
 app.get('/', (req, res) => {
   res.send('Sentry node test');
 });
 
-app.listen(config.port, () => console.log(`App started`));
+app.get('/error', (req, res) => {
+  throw new Error();
+});
+
+app.use(Raven.errorHandler());
+
+app.listen(config.port);
--- config.js
+++ config.js
@@ -1,8 +1,13 @@
 const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
-  username: process.env.USERNAME,
-  password: process.env.PASSWORD
+  username: process.env.USERNAME || 'user',
+  password: process.env.PASSWORD || 'pass',
+  sentry: {
+    nodeDsn: process.env.SENTRY_NODE_DSN,
+    jsDsn: process.env.SENTRY_JS_DSN
+  }
+
 };
 
 module.exports = config;
